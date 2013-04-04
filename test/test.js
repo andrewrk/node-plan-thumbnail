428fe30077af348bc6b79b717abc2024d2ff2c3b
Steve Perkins <steve@indabamusic.com>
2013-04-04 17:08:23 -0400

    test case

---
+++ test/test.js
@@ -0,0 +1,35 @@
+var definition = require('../')
+  , Plan = require('plan')
+  , assert = require('assert')
+  , fs = require('fs')
+  , path = require('path')
+  , makeTemp = require('temp').path
+
+var tmpFilePath = path.join(__dirname, "logo4w.png");
+
+
+describe("s3-download", function() {
+  it("works", function(done) {
+    var task = Plan.createTask(definition, 'thumbnail', 
+      {
+        "format": "png",
+        "width": 220,
+        "height": 220,
+        "crop": true
+      }
+    );
+    var plan = new Plan();
+    plan.addTask(task);
+    plan.on('error', function(err) {
+      throw err;
+    });
+    plan.on('end', function(results) {
+      //console.log(results);
+      done();
+    });
+    plan.start({
+      tmpPath: tmpFilePath,
+      makeTemp: makeTemp
+    });
+  });
+});
