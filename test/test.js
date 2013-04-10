539680561dfc2231c792c33a5607e927c5f99b42
Steve Perkins <steve@indabamusic.com>
2013-04-10 12:47:16 -0400

    test google logo and facebook avatar

--- test/test.js
+++ test/test.js
@@ -5,30 +5,39 @@ var definition = require('../')
   , path = require('path')
   , makeTemp = require('temp').path
 
-var tmpFilePath = path.join(__dirname, "logo4w.png");
 
 
-describe("s3-download", function() {
-  it("works", function(done) {
-    var task = Plan.createTask(definition, 'thumbnail', 
-      {
-        "format": "png",
-        "width": 220,
-        "height": 220,
-        "crop": true
-      }
-    );
-    var plan = new Plan();
-    plan.addTask(task);
-    plan.on('error', function(err) {
-      throw err;
-    });
-    plan.on('end', function(results) {
-      done();
-    });
-    plan.start({
-      tempPath: tmpFilePath,
-      makeTemp: makeTemp
-    });
+describe("thumbnail", function() {
+  it("works with google logo", function(done) {
+    testImage('logo4w.png', done);
+  });
+  it("works with facebook avatar", function(done) {
+    testImage('facebook.gif', done);
   });
 });
+
+
+
+function testImage(name, done) {
+  var tmpFilePath = path.join(__dirname, name);
+  var task = Plan.createTask(definition, 'thumbnail', 
+    {
+      "format": "png",
+      "width": 220,
+      "height": 220,
+      "crop": true
+    }
+  );
+  var plan = new Plan();
+  plan.addTask(task);
+  plan.on('error', function(err) {
+    throw err;
+  });
+  plan.on('end', function(results) {
+    done();
+  });
+  plan.start({
+    tempPath: tmpFilePath,
+    makeTemp: makeTemp
+  });
+}
